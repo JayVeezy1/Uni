d715bf0236c28bdd9be9d5c2d80c826b4b8b2c04
RobertBetschinger <51017906+RobertBetschinger@users.noreply.github.com>
2020-12-07 19:44:42 +0100

    Update aufgabe5.py
    Nur andere Methoden als Ergänzung

--- WiSe20_CRM/Uebung_4_Python_1/aufgabe5.py
+++ WiSe20_CRM/Uebung_4_Python_1/aufgabe5.py
@@ -6,7 +6,7 @@ Created on Sun Dec  6 14:52:18 2020
 """
 import pandas as pd
 
-# wofuer wird das r gebraucht?
+# wofuer wird das r gebraucht?  R steht hier wsl. für "read"
 def aufgabe_5():
     df = pd.read_csv(r'C:\Users\vanek\Desktop\Mitarbeiterdaten.csv',
                      delimiter = ";",
@@ -16,10 +16,10 @@ def aufgabe_5():
     print(df, "\n")
     
     # 2. waehle bestimmte PerNr (index) 
-    print(df[df.index == 124532], "\n")
+    print(df[df.index == 124532], "\n") "Diese Methode hab ich nicht gefunden, habe print(df.loc[124532]) benutzt"
     
     # 3. waehle nur Geb spalte
-    print(df.Geb, "\n")
+    print(df.Geb, "\n") "Schöne Methode, habe nur print(df["Geb"].tolist()) gefunden"
     
     # 4. waehle Jahresgehalt von bestimmtem Namen 
     # iloc fuer row position
@@ -30,11 +30,14 @@ def aufgabe_5():
     # print(df.loc[df.Name == 'Hofstadter', ['Jahresgehalt']], "\n")
     # ueber values auf (einzigen) wert des gefilterten df zugreifen
     print(df.loc[df.Name == 'Hofstadter', ['Jahresgehalt']].values[0][0], "\n")
+    "Meine Methode :print(df["Jahresgehalt"].loc[124532])"
     
     # 5. Addiere zwei Gehaelter  
     hofstadter_gehalt = df.loc[df.Name == 'Hofstadter', ['Jahresgehalt']].values[0][0]
     cooper_gehalt = df.loc[df.Name == 'Cooper', ['Jahresgehalt']].values[0][0]
     print(hofstadter_gehalt + cooper_gehalt)    
+    "andere methode: valCooper =df["Jahresgehalt"].loc[110190]
+                    valHof = df["Jahresgehalt"].loc[139823]"
     
     
 def main():
